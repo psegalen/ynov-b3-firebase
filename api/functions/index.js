41b271ab09661da900747d926c9d9890d4d88c01
Pierre Segalen <pierre.segalen@gmail.com>
2020-02-17 17:05:23 +0100

    Added a route to post a message

--- api/functions/index.js
+++ api/functions/index.js
@@ -14,7 +14,55 @@ exports.getMessages = functions
       .get();
     const messages = [];
     querySnapshot.forEach(doc => {
-      messages.push(doc.data());
+      messages.push(Object.assign({}, doc.data(), { id: doc.id }));
     });
     res.json({ messages });
+  });
+
+exports.postMessage = functions
+  .region("europe-west1")
+  .https.onRequest(async (req, res) => {
+    const message = req.body;
+    if (!message.text || message.text.length === 0) {
+      res.json({ status: "error", error: "Le champ 'text' est obligatoire !" });
+    } else {
+      if (!message.userId || message.userId.length === 0) {
+        res.json({
+          status: "error",
+          error: "Le champ 'userId' est obligatoire !"
+        });
+      } else {
+        // Message OK
+        try {
+          const user = await admin.auth().getUser(message.userId);
+          // User exists
+          try {
+            await admin
+              .firestore()
+              .collection("messages")
+              .add({
+                text: message.text,
+                userId: message.userId,
+                time: admin.firestore.Timestamp.now()
+              });
+            res.json({
+              status: "ok"
+            });
+          } catch (error) {
+            res.json({
+              status: "error",
+              error: "Impossible d'ajouter le message !",
+              details: error.message
+            });
+          }
+        } catch (error) {
+          // User doesn't exist
+          res.json({
+            status: "error",
+            error: "L'utilisateur n'existe pas !",
+            details: error.message
+          });
+        }
+      }
+    }
   });
